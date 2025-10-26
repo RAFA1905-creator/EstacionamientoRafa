786669aae5498b1a66d0ae66fc36093d888c0be0
Rafael <rafaelgl1905@gmail.com>
2025-10-26 02:13:41 +0200

    Clase "Coche".

--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -1,4 +1,38 @@
 package modelo;
 
-public class Coche {
+public class Coche implements Runnable {
+
+    private String nombre;
+    private boolean vip;
+    private Estacionamiento estacionamiento;
+
+    public Coche(String nombre, boolean vip, Estacionamiento estacionamiento) {
+        this.nombre = nombre;
+        this.vip = vip;
+        this.estacionamiento = estacionamiento;
+    }
+
+    @Override
+    public void run() {
+        boolean aparcado = estacionamiento.entrar(this);
+        if (aparcado == true) {
+            try {
+                int tiempo = 2000 + ((int) (Math.random() * 4000));
+                Thread.sleep(tiempo);
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                estacionamiento.salir(this);
+            }
+        }
+    }
+
+    public boolean esVip() {
+        return vip;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + (vip ? " (VIP)" : " (Normal)");
+    }
 }
